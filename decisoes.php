4fa023b3b1892cf2f73e16b9a509d4ed9c591a2d
Biamah <beatriz.souza.costa@outlook.com>
2021-11-23 23:05:44 -0300

    feat: else if

--- decisoes.php
+++ decisoes.php
@@ -1,17 +1,22 @@
 <?php
 
-$idade = 25;
+$idade = 15;
 $nome = "Beatriz";
+$numeroPessoas = 2;
 
 echo "voce so pode entrar se tiver mais que 18 anos." . PHP_EOL;
+echo "a partir de 16 anos acompanhado" . PHP_EOL;
 
 // pode ser usado || ou "or"
 // se não quiser criar duas verificações pode apenas utilizar >= 
-if ($idade == 18 || $idade > 18){
-    echo "voce tem $idade anos pode entrar" . PHP_EOL;
-}
-
 // pode usar o and ou &&
 if ($idade >= 18 && $nome == "Beatriz"){
-    echo "$nome pode entrar!";
+    echo "$nome voce tem $idade anos. Pode entrar sozinho." . PHP_EOL;
+} else if ($idade >= 16 && $numeroPessoas > 1){
+    echo "$nome voce tem $idade anos, mas esta acompanhada. Pode entrar." . PHP_EOL;
+} else {
+    echo "$nome voce tem $idade não pode entrar." . PHP_EOL;
 }
+
+echo PHP_EOL;
+echo "Adeus!";
