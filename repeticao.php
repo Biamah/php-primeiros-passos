1bfdd79df8b3af40504db5d91a510cc51e8fdee6
Biamah <beatriz.souza.costa@outlook.com>
2021-11-23 23:39:13 -0300

    feat: break e continue

--- repeticao.php
+++ repeticao.php
@@ -8,7 +8,17 @@ while ($contador <= 15) {
     $contador ++;
 }
 */
-
+/*
 for ($contador = 1; $contador <= 15; $contador ++) {
-    echo "#$contador" . PHP_EOL;
+    if ($contador == 13){
+        continue;
+    } 
+    echo "#$contador" . PHP_EOL;  
+}
+*/
+for ($contador = 1; $contador <= 15; $contador ++) {
+    if ($contador == 13){
+        break;
+    } 
+    echo "#$contador" . PHP_EOL;  
 }
