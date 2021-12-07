397d281172e894d9e9958a7ba4e5b441acc50a27
Biamah <beatriz.souza.costa@outlook.com>
2021-12-07 19:17:53 -0300

    feat: conta corrente

---
+++ avancando/conta-corrente.php
@@ -0,0 +1,22 @@
+<?php
+
+$conta1 = [
+    'titular' => 'Vinicius',
+    'saldo' => 1000
+];
+
+$conta2 = [
+    'titular' => 'Isabela',
+    'saldo' => 2000
+];
+
+$conta3 = [
+    'titular' => 'Gabriela',
+    'saldo' => 3000
+];
+
+$contasCorrentes = [$conta1, $conta2, $conta3];
+
+for ($i = 0; $i < count($contasCorrentes); $i++) {
+    echo $contasCorrentes[$i]['titular'] . PHP_EOL;
+}
