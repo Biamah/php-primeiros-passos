44cc5b47e8198f03f02cbd7b5b367dfd627b2326
Biamah <beatriz.souza.costa@outlook.com>
2021-12-07 18:05:41 -0300

    docs: organiza docs

---
+++ avancando/listas.php
@@ -0,0 +1,7 @@
+<?php
+
+$idadeLista = [21, 23, 19, 25, 30, 41, 18];
+$primeiraIdade = $idadeLista[3];
+
+echo $primeiraIdade;
+
---
+++ desafios/repeticao.php
@@ -0,0 +1,24 @@
+<?php
+
+$contador = 1;
+
+/*
+while ($contador <= 15) {
+    echo "#$contador" . PHP_EOL;
+    $contador ++;
+}
+*/
+/*
+for ($contador = 1; $contador <= 15; $contador ++) {
+    if ($contador == 13){
+        continue;
+    } 
+    echo "#$contador" . PHP_EOL;  
+}
+*/
+for ($contador = 1; $contador <= 15; $contador ++) {
+    if ($contador == 13){
+        break;
+    } 
+    echo "#$contador" . PHP_EOL;  
+}
