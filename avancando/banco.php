a460a1c6b68c6a67c997aa992b81d815c7c404c8
Biamah <beatriz.souza.costa@outlook.com>
2021-12-08 09:41:48 -0300

    feat: separando funcoes em arquivos independentes

--- avancando/banco.php
+++ avancando/banco.php
@@ -1,30 +1,6 @@
 <?php
 
-function exibeMensagem(string $mensagem) 
-{
-    echo $mensagem . PHP_EOL;
-}
-
-function sacar(array $conta, float $valorASacar) : array
-{
-    if ($valorASacar > $conta['saldo']) {
-        exibeMensagem( mensagem: "Voce não pode sacar!");
-    } else {
-        $conta['saldo'] -= $valorASacar;
-    }
-
-    return $conta;
-}
-
-function depositar(array $conta, float $valorADepositar) : array
-{
-    if ($valorADepositar > 0) {
-        $conta['saldo'] += $valorADepositar;
-    } else {
-        exibeMensagem( mensagem: "Deposito precisa ser positivos");
-    }
-    return $conta;
-}
+require_once 'funcoes.php';
 
 $contasCorrentes = [
     '000.000.000-00' => [
