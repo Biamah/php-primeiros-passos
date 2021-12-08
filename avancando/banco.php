ce4c11016619ca402488f9308fe2174a54db2905
Biamah <beatriz.souza.costa@outlook.com>
2021-12-08 13:51:52 -0300

    feat: variavel por referencia

--- avancando/banco.php
+++ avancando/banco.php
@@ -20,6 +20,8 @@ $contasCorrentes = [
 $contasCorrentes['000.000.000-03'] = sacar($contasCorrentes['000.000.000-03'], valorASacar: 500);
 $contasCorrentes['000.000.000-01'] = depositar($contasCorrentes['000.000.000-01'], valorADepositar: 900);
 
+titularLetrasMaiusculas($contasCorrentes['000.000.000-01']);
+
 foreach ($contasCorrentes as $indice => $conta) {
     exibeMensagem( 
         mensagem:"$indice {$conta['titular']}  {$conta['saldo']}" 
